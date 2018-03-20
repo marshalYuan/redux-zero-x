993a9c65a47ddf947b8682add92ea3418a341895
marshalYuan <marshal_o@163.com>
2018-03-21 02:16:02 +0800

    feat(innerRef): add innerRef api

--- src/connect.tsx
+++ src/connect.tsx
@@ -4,62 +4,12 @@ import { shallowEqual, isFunction, propsValidation } from "./utils"
 // import propValidation from "../../utils/propsValidation"
 // import bindActions from "../../utils/bindActions"
 
-interface P {
+export interface P {
   children: (...args) => any
   stores?: string[]
-  mapStateToProps?: () => Object
+  mapStateToProps?: (...args) => Object
   mapActionToProps?: any
-}
-
-class Connect extends React.Component<P, any> {
-  static contextTypes = {
-    stores: propsValidation
-  }
-  unsubscribe: Function[]
-  stores = this.getStores()
-  state = this.getProps()
-  actions = this.getActions()
-  componentWillMount() {
-    this.unsubscribe = this.stores.map(s => s.subscribe(this.update))
-  }
-  componentWillUnmount() {
-    this.unsubscribe.forEach(f => f(this.update))
-  }
-  getStores() {
-    const { stores } = this.props
-    if (!stores) {
-      return Array.isArray(this.context.stores)
-        ? this.context.stores
-        : Object.keys(this.context.stores).map(key => this.context.stores[key])
-    }
-    return stores.map(key => {
-      if (!(key in this.context.stores)) {
-        throw new Error(`Can't find ${key} in Provider's props`)
-      }
-      return this.context.stores[key]
-    })
-  }
-  getProps() {
-    const { mapStateToProps } = this.props
-    return mapStateToProps(...this.stores.map(s => s.getState()), this.props)
-  }
-  getActions() {
-    const { mapActionToProps } = this.props
-    return mapActionToProps(...this.stores.map(s => s.getActions()), this.props)
-  }
-  update = () => {
-    const mapped = this.getProps()
-    if (!shallowEqual(mapped, this.state)) {
-      this.setState(mapped)
-    }
-  }
-  render() {
-    return this.props.children({
-      stores: this.stores,
-      ...this.state,
-      ...this.actions
-    })
-  }
+  innerRef?: (ref: React.ReactElement<any>) => any
 }
 
 const defaultMapStateToProps = (...args) => {
@@ -73,12 +23,12 @@ const defaultMapStateToProps = (...args) => {
 
 const defaultMapActionToProps = defaultMapStateToProps
 
-export default function connect(): any
-export default function connect(
+export function connect(): any
+export function connect(
   mapStateToProps,
   mapActionToProps?: Function
 ): any
-export default function connect(
+export function connect(
   stores: string[],
   mapStateToProps?: Function,
   mapActionToProps?: Function
@@ -91,14 +41,58 @@ export default function connect(arg0?: string[] | Function, arg1?, arg2?) {
       ? arg1 || defaultMapActionToProps
       : arg2 || defaultMapActionToProps
   ]
-  return Child => props => (
-    <Connect
-      {...props}
-      stores={stores}
-      mapStateToProps={mapStateToProps}
-      mapActionToProps={mapActionToProps}
-    >
-      {mappedProps => <Child {...mappedProps} {...props} />}
-    </Connect>
-  )
+  return (Child) => {
+
+    return class ConnectWrapper extends React.Component<P, any> {
+      static displayName = `ConnectWrapper(${Child.displayName || Child.name || "Component"})`
+      static contextTypes = {
+        stores: propsValidation
+      }
+      unsubscribe: Function[]
+      stores = this.getStores()
+      state = this.getProps()
+      actions = this.getActions()
+      componentWillMount() {
+        this.unsubscribe = this.stores.map(s => s.subscribe(this.update))
+      }
+      componentWillUnmount() {
+        this.unsubscribe.forEach(f => f(this.update))
+      }
+      getStores() {
+        const { stores } = this.props
+        if (!stores) {
+          return Array.isArray(this.context.stores)
+            ? this.context.stores
+            : Object.keys(this.context.stores).map(key => this.context.stores[key])
+        }
+        return stores.map(key => {
+          if (!(key in this.context.stores)) {
+            throw new Error(`Can't find ${key} in Provider's props`)
+          }
+          return this.context.stores[key]
+        })
+      }
+      getProps() {
+        return mapStateToProps(...this.stores.map(s => s.getState()), this.props)
+      }
+      getActions() {
+        return mapActionToProps(...this.stores.map(s => s.getActions()), this.props)
+      }
+      update = () => {
+        const mapped = this.getProps()
+        if (!shallowEqual(mapped, this.state)) {
+          this.setState(mapped)
+        }
+      }
+
+      setWrappedInstance = ref => {
+        if (isFunction(this.props.innerRef))
+          this.props.innerRef(ref)
+      }
+
+      render() {
+        return React.createElement(Child, {stores: this.stores, ref: this.setWrappedInstance, ...this.props, ...this.state, ...this.actions})
+      }
+    }
+  }
 }
